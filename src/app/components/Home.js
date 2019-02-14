b17025c129bd851e33dd34c62fa7d29ed8167c5e
Scott Hsieh <scott@one45.com>
2019-02-14 09:39:06 -0800

    adding buttons

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 export class Home extends React.Component {
+
+    constructor(props) {
+        super();
+        this.size = props.size;
+    }
+
+    onFeed() {
+    this.size += 20;
+    }
+
+    onStarve() {
+      this.size -= 20;
+    }
+
     render() {
         const image = require.context('../../../assets', true);
         const pet_img = image(`./${this.props.name}.png`); // with require
-
+        const size = this.size;
         return (
             <div>
                 <h3>This is your pet: {this.props.name}!</h3>
-                 <img src={pet_img} height={this.props.size} width={this.props.size}/>
-                <p>You're almost {this.props.size}px big</p>
+                <img src={pet_img} height={size} width={size}/>
+                <p>You're almost {size}px big</p>
+                <button className="btn btn-danger" onClick={this.onStarve.bind(this)}>- Starve </button>
+                &nbsp;
+                <button className="btn btn-success" onClick={this.onFeed.bind(this)}>+ Feed </button>
             </div>
         );
     }
