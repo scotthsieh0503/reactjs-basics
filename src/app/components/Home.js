c8d2ef0531a40a7812d9405da7fb3067897840d2
Scott Hsieh <scott@one45.com>
2019-02-14 11:18:44 -0800

    Data binding

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,7 +5,8 @@ export class Home extends React.Component {
     constructor(props) {
         super();
         this.state = {
-             size: props.size
+             size: props.size,
+             name: props.name
          };
     }
 
@@ -21,13 +22,26 @@ export class Home extends React.Component {
              });
     }
 
+    onHandleChange(event) {
+      this.setState({
+                 name: event.target.value
+               });
+    }
+
     render() {
         const image = require.context('../../../assets', true);
-        const pet_img = image(`./${this.props.name}.png`); // with require
-        
+        const pet_img = image(`./${this.state.name}.png`); // with require
+
         return (
             <div>
-                <h3>This is your pet: {this.props.name}!</h3>
+                <select className="form-control" id="pets" onChange={(event) => this.onHandleChange(event)} value={this.state.name}>
+                   <option value="kau">kau</option>
+                   <option value="aisha">aisha</option>
+                   <option value="acara">acara</option>
+                   <option value="lenny">lenny</option>
+                </select>
+
+                <h3>This is your pet: {this.state.name}!</h3>
                 <img src={pet_img} height={this.state.size} width={this.state.size}/>
                 <p>You're almost {this.state.size}px big</p>
                 <button className="btn btn-danger" onClick={this.onStarve.bind(this)}>- Starve </button>
