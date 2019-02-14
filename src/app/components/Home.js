965d689d71080487de82545c2a1f8b0518855bcb
Scott Hsieh <scott@one45.com>
2019-02-14 09:50:20 -0800

    add event and states

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,26 +4,33 @@ export class Home extends React.Component {
 
     constructor(props) {
         super();
-        this.size = props.size;
+        this.state = {
+             size: props.size
+         };
     }
 
     onFeed() {
-    this.size += 20;
+      this.setState({
+                 size: this.state.size + 20
+             });
     }
 
     onStarve() {
-      this.size -= 20;
+      this.setState({
+                 size: this.state.size - 20
+             });
     }
 
     render() {
         const image = require.context('../../../assets', true);
         const pet_img = image(`./${this.props.name}.png`); // with require
-        const size = this.size;
+        const size = this.state.size;
+        console.log(size);
         return (
             <div>
                 <h3>This is your pet: {this.props.name}!</h3>
-                <img src={pet_img} height={size} width={size}/>
-                <p>You're almost {size}px big</p>
+                <img src={pet_img} height={this.state.size} width={this.state.size}/>
+                <p>You're almost {this.state.size}px big</p>
                 <button className="btn btn-danger" onClick={this.onStarve.bind(this)}>- Starve </button>
                 &nbsp;
                 <button className="btn btn-success" onClick={this.onFeed.bind(this)}>+ Feed </button>
