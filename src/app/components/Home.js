7f64a3693e773636dce71978161738f5fda791a3
Scott Hsieh <scott@one45.com>
2019-02-14 21:37:46 -0800

    update function handler

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -9,13 +9,13 @@ export class Home extends React.Component {
          };
     }
 
-    onFeed() {
+    onFeed = () => {
       this.setState({
                  size: this.state.size + 20
              });
     }
 
-    onStarve() {
+    onStarve = () => {
       this.setState({
                  size: this.state.size - 20
              });
@@ -24,15 +24,15 @@ export class Home extends React.Component {
     render() {
         const image = require.context('../../../assets', true);
         const pet_img = image(`./${this.props.name}.png`); // with require
-        
+
         return (
             <div>
                 <h3>This is your pet: {this.props.name}!</h3>
                 <img src={pet_img} height={this.state.size} width={this.state.size}/>
                 <p>You're almost {this.state.size}px big</p>
-                <button className="btn btn-danger" onClick={this.onStarve.bind(this)}>- Starve </button>
+                <button className="btn btn-danger" onClick={this.onStarve}>- Starve </button>
                 &nbsp;
-                <button className="btn btn-success" onClick={this.onFeed.bind(this)}>+ Feed </button>
+                <button className="btn btn-success" onClick={this.onFeed}>+ Feed </button>
             </div>
         );
     }
